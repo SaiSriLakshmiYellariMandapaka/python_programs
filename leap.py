090037ae8deba0bcfbd41927a19c0c3a3aad54f4
SaiSriLakshmiYellariMandapaka <srilakshmimandapaka@gmail.com>
2020-05-11 12:22:14 +0530

    Add files via upload

---
+++ leap.py
@@ -0,0 +1,20 @@
+'''#In the Gregorian calendar three criteria must be taken into account to identify leap years:
+
+    The year can be evenly divided by 4, is a leap year, unless:
+        The year can be evenly divided by 100, it is NOT a leap year, unless:
+            The year is also evenly divisible by 400. Then it is a leap year.'''
+def leap_fn(year):
+    leap = False
+    
+    # Write your logic here
+    if year % 400 == 0:
+        leap = True
+    elif year % 100 == 0:
+        leap = False
+    elif year % 4 == 0:
+        leap = True
+    
+    return leap
+    
+year = int(input('enter a year'))
+print (leap_fn(year))
