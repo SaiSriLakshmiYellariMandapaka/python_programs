d4a10b18316fe66fb9c5a69baa9ab3aeaa99bdff
SaiSriLakshmiYellariMandapaka <srilakshmimandapaka@gmail.com>
2020-06-11 11:11:25 +0530

    Add files via upload

---
+++ mad_libs_game.py
@@ -0,0 +1,12 @@
+#Simple Mad Libs Game
+''' Mad Libs is a phrasal template word game where one player prompts others for a list of words to substitute for blanks
+ in a story before reading aloud. The game is frequently played as a party game or as a pastime.'''
+
+color = input("Enter a color : ")
+plural_noun = input("Enter a plural noun : ")
+celebrity = input("Enter a celebrity : ")
+
+
+print("Roses are " +color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
---
+++ playing_audio_files.py
@@ -0,0 +1,4 @@
+#Playing audio files using python
+
+import playsound
+playsound.playsound("senorita.mp3")
